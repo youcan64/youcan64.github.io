843ed9d06ddd6dd4cd8f3c9153e3394270335e9d
youcan <ysk928@gmail.com>
2025-01-11 18:03:02 +0900

    import react if required

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Landing from './component/Landing'
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { ThemeProvider } from '@mui/material/styles'
 import theme from './theme'
